64e3447f8ef60b59da015e38e0704cbd5a696532
BraedenMcAllister <78716203+BraedenMcAllister@users.noreply.github.com>
2021-02-17 15:19:39 +0800

    Add files via upload

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public float damage = 20;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/SecondMovingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondMovingPlatform : MonoBehaviour
+{
+    public Transform[] points;
+    public float speed = 0;
+
+    private Transform currentTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(points.Length > 0)
+        {
+            currentTarget = points[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTarget != null)
+        { 
+           if(Vector3.Distance(transform.position, currentTarget.position)< 0.5f)
+            {
+                index++;
+                if(indexer >= points.Length)
+            }
+        }
+    }
+}
